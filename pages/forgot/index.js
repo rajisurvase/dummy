e277a38361fcc003b56230f15f4d46dafadfba6b
anjali <naragudeav@gmail.com>
2023-04-11 23:17:57 +0530

    working on forgot password

--- pages/forgot/index.js
+++ pages/forgot/index.js
@@ -1,9 +1,49 @@
- 
- import React from 'react'
-const Forgot=()=>{
 
-    return(
-<div> Forgot</div>
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
+import Link from 'next/link'
+import React, { useState } from 'react'
+const Forgot = () => {
+    const [userInfo,setUserInfo] =useState({email: ""})
+    function handleSubmit(){
+    // setUserInfo(userInfo?.email)
+    console.log(userInfo?.email)
+    }
+
+    return (
+
+
+        <div>
+
+            <Box >
+                <Grid container spacing={2}>
+                    < Grid item xs={12} md={4}></Grid>
+                    <Grid item xs={12} md={4} textAlign={"center"} padding={3}>
+                        <form onSubmit={handleSubmit}>
+                            <Card >
+                                <CardContent>
+                                    <Typography p={3} >Forgot your password ?</Typography>
+
+                                    <TextField fullWidth id="standard-basic" sx={{ mt: 2 }} type="email" label="Email" variant="standard" value={userInfo.email}
+                                        onChange={({ target }) =>
+                                            setUserInfo({ ...userInfo, email: target.value })
+                                        } />
+
+
+                                    <Button fullWidth sx={{ mt: 3 }} variant="contained" type="submit">Send</Button>
+                                    <Typography sx={{ mt: 2 }} textAlign="center">
+                                        <Link href="/login" >Back to Login Page</Link></Typography>
+                                </CardContent>
+                            </Card>
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} md={4}></Grid>
+                </Grid>
+            </Box>
+
+
+
+        </div>
+
     )
 }
 export default Forgot
